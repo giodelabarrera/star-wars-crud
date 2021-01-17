3c81700f23a985864c1a72be34715cf40945c3b9
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-01-17 22:15:35 +0100

    Add styles for info list

--- src/components/character/preview/index.tsx
+++ src/components/character/preview/index.tsx
@@ -24,33 +24,33 @@ export default function CharacterPreview({character}: CharacterPreviewProps) {
   return (
     <div className={baseClass} data-id={id}>
       <h2>{name}</h2>
-      <ul>
+      <ul className={`${baseClass}-info`}>
         <li>
-          <strong>Birth year:</strong>
+          <strong>Birth year: </strong>
           {birthYear}
         </li>
         <li>
-          <strong>Gender:</strong>
+          <strong>Gender: </strong>
           {gender}
         </li>
         <li>
-          <strong>Height:</strong>
+          <strong>Height: </strong>
           {height}
         </li>
         <li>
-          <strong>Mass:</strong>
+          <strong>Mass: </strong>
           {mass}
         </li>
         <li>
-          <strong>Hair color:</strong>
+          <strong>Hair color: </strong>
           {hairColor}
         </li>
         <li>
-          <strong>Skin color:</strong>
+          <strong>Skin color: </strong>
           {skinColor}
         </li>
         <li>
-          <strong>Eye color:</strong>
+          <strong>Eye color: </strong>
           {eyeColor}
         </li>
       </ul>
