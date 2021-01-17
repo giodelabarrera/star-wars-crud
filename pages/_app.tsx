b3563135a432e6d2b89f7764b58872d12f7afea8
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-01-17 01:31:17 +0100

    Add theme and global styles

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import '../styles/theme.scss'
+import '../styles/global.scss'
+
+export default function App({Component, pageProps}) {
+  return <Component {...pageProps} />
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,3 @@
 export default function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return <h1>Welcome to Next.js!</h1>
 }
