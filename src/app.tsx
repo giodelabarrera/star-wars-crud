a97579f7783b8682bf843b4d23fb594617c11445
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-01-17 16:08:55 +0100

    Create character list screen

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
 
 import Header from './components/layout/header'
+
+import CharacterListScreen from './screens/characterList'
+
 import './app.scss'
 
 function App() {
   return (
     <div className="sw-App">
       <Header />
-      <main>Welcome</main>
+      <main>
+        <AppRoutes />
+      </main>
     </div>
+  )
+}
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<CharacterListScreen />} />
+    </Routes>
   )
 }
 
