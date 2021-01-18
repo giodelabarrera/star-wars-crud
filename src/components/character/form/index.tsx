15c2267f079f84e259369cf64140029adb39f45f
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-01-18 16:12:17 +0100

    Create label component for field group

--- src/components/character/form/index.tsx
+++ src/components/character/form/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 import {Character} from '../../../types'
+import Input from '../../ui/input'
+import Label from '../../ui/label'
+
+import './index.scss'
+
+const baseClass = 'sw-CharacterForm'
 
 type CharacterFormProps = {
   character?: Character
@@ -17,5 +23,46 @@ export default function CharacterForm({
     onSubmit(data)
   }
 
-  return <form onSubmit={handleSubmit}></form>
+  return (
+    <form className={baseClass} onSubmit={handleSubmit}>
+      <div className={`${baseClass}-fieldGroup`}>
+        <h3>Identity</h3>
+        <div className={`${baseClass}-textField`}>
+          <Label htmlFor="name">Name</Label>
+          <Input type="text" id="name" />
+        </div>
+        <div className={`${baseClass}-textField`}>
+          <Label htmlFor="birth-year">Birth year</Label>
+          <Input type="text" id="birth-year" />
+        </div>
+        <div className={`${baseClass}-textField`}>
+          <Label htmlFor="gender">Gender</Label>
+          <Input type="text" id="gender" />
+        </div>
+      </div>
+      <div className={`${baseClass}-fieldGroup`}>
+        <h3>Characteristics</h3>
+        <div className={`${baseClass}-textField`}>
+          <Label htmlFor="height">Height</Label>
+          <Input type="text" id="height" />
+        </div>
+        <div className={`${baseClass}-textField`}>
+          <Label htmlFor="mass">Mass</Label>
+          <Input type="text" id="mass" />
+        </div>
+        <div className={`${baseClass}-textField`}>
+          <Label htmlFor="hair-color">Hair color</Label>
+          <Input type="text" id="hair-color" />
+        </div>
+        <div className={`${baseClass}-textField`}>
+          <Label htmlFor="skin-color">Skin color</Label>
+          <Input type="text" id="skin-color" />
+        </div>
+        <div className={`${baseClass}-textField`}>
+          <Label htmlFor="eye-color">Eye color</Label>
+          <Input type="text" id="eye-color" />
+        </div>
+      </div>
+    </form>
+  )
 }
