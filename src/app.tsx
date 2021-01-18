10855051f0a64b41c43454aa132b3b4652689156
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-01-18 15:38:21 +0100

    Create character create screen

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,7 @@ import {Routes, Route} from 'react-router-dom'
 import Header from './components/layout/header'
 
 import CharacterListScreen from './screens/characterList'
+import CharacterCreateScreen from './screens/characterCreate'
 
 import './app.scss'
 
@@ -22,6 +23,7 @@ function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<CharacterListScreen />} />
+      <Route path="/create" element={<CharacterCreateScreen />} />
     </Routes>
   )
 }
---
+++ src/screens/characterCreate/index.tsx
@@ -0,0 +1,13 @@
+import Paper from '../../components/ui/paper'
+
+const baseClass = 'sw-CharacterListScreen'
+
+export default function CharacterCreateScreen() {
+  return (
+    <div className={baseClass}>
+      <Paper>
+        <h1>Hola</h1>
+      </Paper>
+    </div>
+  )
+}
