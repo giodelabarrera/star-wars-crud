2421b076151c688022a15c37878deb4dab6e7e7f
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-01-19 20:15:03 +0100

    Add not found screen

--- src/screens/notFound/index.tsx
+++ src/screens/notFound/index.tsx
@@ -7,16 +7,15 @@ const baseClass = 'sw-NotFoundScreen'
 export default function NotFoundScreen() {
   return (
     <div className={baseClass}>
-      <h1 className={`${baseClass}-title`}>404</h1>
-
       <div className={`${baseClass}-imageContainer`}>
         <Stormtrooper />
       </div>
+      <h1 className={`${baseClass}-title`}>404</h1>
       <div className={`${baseClass}-description`}>
         <p>
           I think we missed our target...
           <br />
-          Move along, boys. This is not the page we're looking for
+          Move along, boys. This is not the page we're looking for.
         </p>
       </div>
     </div>
