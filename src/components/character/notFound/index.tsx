7a2d086818106e887a2098db56e7fcab8cd40d01
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-01-19 20:56:14 +0100

    Add styles for character not found

--- src/components/character/notFound/index.tsx
+++ src/components/character/notFound/index.tsx
@@ -1,10 +1,12 @@
+import './index.scss'
+
 const baseClass = 'sw-CharacterNotFound'
 
 export default function CharacterNotFound() {
   return (
     <div className={baseClass}>
       <p className={`${baseClass}-message`}>
-        There was an error, Character not found
+        There was an error, the character has not been found
       </p>
     </div>
   )
