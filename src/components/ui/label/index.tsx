adcb9d9081d13189deb7ef6fbc6f3c0fd5194a05
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-01-19 11:34:47 +0100

    Add classname for ui label

--- src/components/ui/label/index.tsx
+++ src/components/ui/label/index.tsx
@@ -1,7 +1,15 @@
+import cx from 'classnames'
+
 import './index.scss'
 
 const baseClass = 'sw-uiLabel'
 
-export default function Label(props) {
-  return <label {...props} className={baseClass} />
+export default function Label({
+  className: classNameProp,
+  ...restProps
+}: React.DetailedHTMLProps<
+  React.LabelHTMLAttributes<HTMLLabelElement>,
+  HTMLLabelElement
+>) {
+  return <label className={cx(baseClass, classNameProp)} {...restProps} />
 }
